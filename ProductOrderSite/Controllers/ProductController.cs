1aed6e54985ad20f275f95b58c2d0c02678511f4
Salem <salemandreus@gmail.com>
2018-11-22 12:23:17 +0200

    removed delete and edit functionality from Products

--- ProductOrderSite/Controllers/ProductController.cs
+++ ProductOrderSite/Controllers/ProductController.cs
@@ -59,37 +59,6 @@ namespace ProductOrderSite.Controllers
             return View(product);
         }
 
-        // GET: Product/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Product/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,Price")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(product);
-        }
-
         // GET: Product/Delete/5
         public ActionResult Delete(int? id)
         {
